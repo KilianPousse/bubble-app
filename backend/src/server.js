df6b753f5e1c87086853a187e8de055aae928750
s2200959 <157494327+KilianPousse@users.noreply.github.com>
2025-10-26 12:39:17 +0100

    refactor: env.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js'
 import msgRoutes from './routes/msg.route.js'
 import { connectDB } from './lib/db.js';
-
-dotenv.config();
+import { ENV } from './lib/env.js';
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+}));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/msg", msgRoutes);
 
-if(process.env.NODE_ENV == "production") {
+if(ENV.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*", (_, res) => {
